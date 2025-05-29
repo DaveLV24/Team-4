771eb9ced7dbed32cacd03ca4c50641e83e64912
Arthur <olehno.arturs@gmail.com>
2025-05-29 14:59:33 +0300

    TC-006-AO Hot fix

--- src/test/java/stepDefinitions/SearchSteps.java
+++ src/test/java/stepDefinitions/SearchSteps.java
@@ -15,8 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class SearchSteps {
 
@@ -174,7 +173,7 @@ public class SearchSteps {
         }
         List<Double> expectedList = new ArrayList<>(doubleListWithoutSort);
         Collections.sort(expectedList);
-        assertEquals(expectedList, doubleListWithoutSort);
+        assertNotEquals(expectedList, doubleListWithoutSort);
     }
 
     @Then("^Check for the existence of pagination buttons$")
